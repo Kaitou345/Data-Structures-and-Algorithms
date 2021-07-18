71b5bda0d0f1e72d90d10d6eb13d68feac5c6d21
Kaitou345 <68972628+Kaitou345@users.noreply.github.com>
2021-07-19 04:28:41 +0600

    Update and rename linear_search.cpp to linear_search.h

--- linear_search.h
+++ linear_search.h
@@ -23,12 +23,3 @@ void verify(int index)
         std::cout << "TRUE" << std::endl;
     
 }
-
-
-int main()
-{
-    std::array<int, 10> arr = {1,2,3,4,5,6,7,8,9,10};
-
-    int index = linear_search<int, arr.size()>(arr, 12);  
-    verify(index);
-}
